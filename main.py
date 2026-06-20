e4ee46e82c35f6781193d186e39edb0802d6b202
Miningsleuth <166779773+Miningsleuth@users.noreply.github.com>
2026-06-20 13:08:12 +1200

    Developing starting and json reading

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 """Computer Companion."""
+import json
+
+with open("data/first.json", "r") as f:
+    returning = json.load(f)
+
 
 DEFAULT_APPS = ["Powershell", "Command Prompt", "Settings",
                 "Device Manager", "Task Manager"]
 
-def welcome():
+def welcome_first():
+    """Do greeting for first time user."""
     print("Welcome to Computer Companion!")
+
+
+def welcome_returning():
+    """Do greeting for returning user."""
+    print("Welcome Back!")
+
+
+if returning == 1:
+    welcome_returning()
+else:
+    welcome_first()
+    with open("data/first.json", "w") as f:
+        json.dump(1, f)
