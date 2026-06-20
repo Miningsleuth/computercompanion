ca13329af2b194f9a216c2b42e946f70086f48bf
Miningsleuth <166779773+Miningsleuth@users.noreply.github.com>
2026-06-20 16:49:03 +1200

    pinging started

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """Computer Companion."""
 import json
 import os
+import sys
+import time
 
+try:
+    from pythonping import ping
+except ImportError:
+    print("To use ping utilities, you need to install the pythonping "
+    "library. \nPlease run: pip install pythonping in your terminal.")
 DEFAULT_APPS = {"Powershell": "powershell",
                 "Command Prompt": "cmd",
                 "Cmd": "cmd",
@@ -11,7 +18,7 @@ DEFAULT_APPS = {"Powershell": "powershell",
                 "Task Manager": "taskmgr",
                 "TM": "taskmgr"}
 request = ""
-
+ping_ip = []
 
 def file_setup():
     """Do a check if .json file exists, and if not creates them."""
@@ -39,12 +46,12 @@ def welcome_first():
         json.dump(1, f)
     with open("data/name.json", "w") as f:
         json.dump(name, f)
-    print(f"Nice to meet you, {name}!")
+    print(f"Nice to meet you, {name}!\n")
 
 
 def welcome_returning(name):
     """Do greeting for returning user."""
-    print(f"Welcome Back, {name}!")
+    print(f"Welcome Back, {name}!\n")
 
 
 def app_opener():
@@ -58,7 +65,57 @@ def app_opener():
             return
     print("App not found.")
 
-
+def ping_utility():
+    valid_ip_count = False
+    valid_ping_count = False
+    while valid_ip_count is False:
+        ping_ip_counts = input("How many IP Addresses would you like to"
+                               " ping simultaneously? ")
+        try:
+            ping_ip_counts = int(ping_ip_counts)
+            if ping_ip_counts < 1 or ping_ip_counts > 32:
+                print("Please enter a number greater than 0 and less than or equal to 32.")
+            else:
+                valid_ip_count = True
+        except ValueError:
+            print("Please enter a valid number.")
+    while valid_ping_count is False:
+        ping_times = input("How many times should I ping? ")
+        try:
+            ping_times = int(ping_times)
+            if ping_times < 1 or ping_times > 200:
+                print("Please enter a number greater than 0 and less than or equal to 200.")
+            else:
+                valid_ping_count = True
+        except ValueError:
+            print("Please enter a valid number.")
+    for i in range(ping_ip_counts):
+        ping_ip.append(input(f"Please enter IP Address {i + 1}: "))
+    for i in range(ping_times):
+        time.sleep(1)
+        for i in range(len(ping_ip)):
+            try:
+                response = ping(ping_ip[i], count = 1, verbose=True)
+                if response.success():
+                    print(f"\033[32m\n{ping_ip[i]} is online!\033[0m")
+                else:
+                    print(f"\033[31m\n{ping_ip[i]} is offline.\033[0m")
+            except PermissionError:
+                print("\n[ERROR] Pure Python ping requires Administrator/Root privileges.")
+                print("Please run this terminal/script as Administrator.")
+                break
+            except RuntimeError:
+                print(f"\n[ERROR] Could not ping {ping_ip[i]}. Please check the IP address and try again.")
+                break
+def utilities():
+    """Display other utilities."""
+    utility_request = ""
+    while utility_request != "clear":
+        print("Utilities: Clear - Clear the chat, C - Calculator, F - Fun,"
+            "R - Random Generators, S - System Info, P - Ping")
+        utility_request = input("Which utility would you like to use? ").lower()
+        if utility_request == "p":
+            ping_utility()
 """Actual program starts here."""
 if returning == 1:
     welcome_returning(name)
@@ -67,7 +124,20 @@ else:
 
 while request != "exit" and request != "quit":
     print("Functions: O - Open an app, Exit - Exit the program, "
-          "Help - Help menu")
+          "U - Other Utilities, Help - Help menu")
     request = input("What would you like to do? ").lower()
     if request == "o":
         app_opener()
+    elif request == "u":
+        utilities()
+    elif request == "help":
+        print("You asked for help, and I will provide!"
+              "I am your computer companion. Tell me a"
+              " command, and I will do it. Here "
+              "are the commands you can use: "
+              "Functions: O - Open an app, Exit - Exit the program, "
+              "U - Other Utilities, Help - Help menu")
+    elif request == "exit" or request == "quit":
+        print("Goodbye!")
+    else:
+        print("That is not a valid command. Please try again.")
